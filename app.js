fb7306acdb03f3618e77b612c824e0187c13753e
JUWON LEE <leeez0128@gmail.com>
2024-02-28 14:47:14 +0900

    Update application response message

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Hello World! CI/CD Completed!!"));
 app.listen(3000, () => console.log("Server ready"));
